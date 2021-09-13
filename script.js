f9a793169c2651708de47bb23b267222e82c535c
ria10 <ria1099@gmail.com>
2021-09-13 16:00:08 +0100

    bug in script.js that doesn't produce correct output

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
- 
+const submitBtn = document.querySelector('.submit-btn');
+const textInput = document.querySelector('#inputJournal');
+
+
+const getPost = async (e) => {
+    e.preventDefault;
+    try {
+        const post = textInput.value;
+        const data = await fetch('https://localhost:3000/posts', 
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                'text': post
+            }),
+            headers: { 
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(data);
+        
+    } catch (err) {
+        console.warn(err);
+    }
+}
+submitBtn.addEventListener('click', async (e) => {
+    e.preventDefault;
+    try {
+        const post = textInput.value;
+        const data = await fetch('https://localhost:3000/posts', 
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                'text': post
+            }),
+            headers: { 
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(data);
+        
+    } catch (err) {
+        console.warn(err);
+    }
+}
+);
