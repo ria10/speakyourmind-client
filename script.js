d7c51d22edcb7d905f90eda161660104d4c24db8
dimtaiwo <dimejitaiwo@Dimejis-MacBook-Pro.local>
2021-09-13 18:12:17 +0100

    Loading all entries

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
       const submitBtn = document.querySelector('.submit-btn');
       const textInput = document.querySelector('#inputJournal')
+      const baseUrl = 'http://localhost:3000'
 //   const form = document.querySelector('form')
 
 //   (async () => {
@@ -20,7 +21,7 @@
 
   submitBtn.addEventListener('click', async(e)=>{
       e.preventDefault();
-      const rawResponse = await fetch('http://localhost:3000/posts', {
+      const rawResponse = await fetch(`${baseUrl}/posts`, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -31,7 +32,38 @@
     const content = await rawResponse.json();
   
     console.log(content);
+
+    let h3 = document.createElement('h3') 
+    h3.textContent = content.text
+    document.getElementById('container').appendChild(h3);
+
+    let p = document.createElement('p')
+
+    p.textContent = content.date
+    document.getElementById('container').appendChild(p)
+
+
+    
   })
+
+  window.onload = async function (){
+      const rawResponse  = await fetch('http://localhost:3000/posts')
+      const content = await rawResponse.json()
+
+      console.log(content)
+
+      for (post of content){
+        let h3 = document.createElement('h3') 
+        h3.textContent = post.text
+        let p = document.createElement('p')
+        p.textContent = post.date
+        document.getElementById('container').append(h3)
+        document.getElementById('container').append(p)   
+
+      }
+     
+
+  }
 
 
 
